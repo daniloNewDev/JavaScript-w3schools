e9478d7116b5e713b7220acecb6996e139e6cd11
Dan Oliveira <danilo.cebolino@gmail.com>
2022-04-30 12:04:52 -0300

    commit map

--- 09_map/script.js
+++ 09_map/script.js
@@ -6,3 +6,14 @@ document.getElementById("exemplo").innerHTML = num2
 function myFunction(value, index, array) {
      return value * 2
 }
+//O método 'map()' contém pares 'key-value em que essas keys podem ser de qualquer tipo de valor;
+
+//É possível criar um map JavaScript:
+
+//        passando um array para "new Map()";
+const frutas = new Map([
+     ["maça", 500],
+     ["banana", 300],
+     ["laranja", 300]
+])
+//        Criando um map usando o "Map.set()";
