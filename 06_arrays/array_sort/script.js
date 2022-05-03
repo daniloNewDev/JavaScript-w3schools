ced9a0cae24ccc08f9f4bc8270480368efe5b64d
Danilo Leite <danilo.cebolino@gmail.com>
2022-05-03 15:08:51 -0300

    Commit Math

--- 06_arrays/array_sort/script.js
+++ 06_arrays/array_sort/script.js
@@ -10,10 +10,10 @@ console.log(frutasEmOrdem.reverse())
 const num = [231, 49, 5, 2, 56, 82]
 console.log(num.sort())
 //Neste exemplo abaixo o item 231 aparece no index 1 porque o método 'sort()' analisa o primeiro index como parâmetro de ordem alfabética;
-const numOrder = num.sort(function(a, b){return a - b})
+const numOrder = num.sort(function (a, b) { return a - b })
 console.log(numOrder)
 //ou então de forma decrescente:
-const numDecres = num.sort(function(a, b){return b - a})
+const numDecres = num.sort(function (a, b) { return b - a })
 console.log(numDecres)
 
 //       Função numérica de comparação do 'sort()':
@@ -25,20 +25,20 @@ console.log(numDecres)
 
 //Não funções incorporadas para encotrar o valor máximo ou mínimo em um Array. Entretanto, depois de classificar o Array é possível obter o valor mais alto e o mais baixo:
 const numeros = [39, 18, 394, 36, 29, 135, 1, 900]
-numeros.sort(function(a, b){return a - b})
+numeros.sort(function (a, b) { return a - b })
 //Menor valor:
 console.log(numeros[0])
 //Maior valor:
-numeros.sort(function(a, b){return b - a})
+numeros.sort(function (a, b) { return b - a })
 console.log(numeros[0])
 
 //
 
 //Classificando Array de objetos:
 const carros = [
-   {marca:"GMC", ano:2016},
-   {marca:"VW", ano:2010},
-   {marca:"Audi", ano:2018}
+   { marca: "GMC", ano: 2016 },
+   { marca: "VW", ano: 2010 },
+   { marca: "Audi", ano: 2018 }
 ]
-carros.sort(function(a, b){return a.ano - b.ano})
+carros.sort(function (a, b) { return a.ano - b.ano })
 console.log(carros)
---
+++ 08_Math/random_math/script.js
@@ -0,0 +1,13 @@
+////////       'Math.random()'          ////////
+
+//Retorna um número aleatório entre 0(inclusive) e 1(exclusive):
+console.log(Math.random())
+//Random sempre retorna um númeor entre 0 e 1;
+//Neste caso falamos sobre números sem decimais e Random.
+
+
+////////       Random Intergers         ////////
+
+
+//'Math.random()' usado com 'Math.floor()' pode ser usado para retornar números inteiros:
+Math.floor(Math.random() * 10)
