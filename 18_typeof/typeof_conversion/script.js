9a104279caa7d75e837eef7dbd8b207102044643
Danilo Leite <danilo.cebolino@gmail.com>
2022-05-09 16:35:28 -0300

    add novo material de estudo javascript

---
+++ 18_typeof/typeof_conversion/script.js
@@ -0,0 +1,41 @@
+//      Type Convertion     //
+
+//    Convertes Strings em Numbers:
+
+//  O método global 'Number()' pode converter strings em Numbers;
+
+console.log(Number("3.14"));
+//  contendo números, converte para Number;
+console.log(Number(" "));
+console.log(Number(""));
+//  Strings vazias são convertidas em 0;
+console.log(Number("23 00"));
+//  Qualquer outro valor é convertido em 'NaN';
+let y = "7"; //  Operador Unary
+let z = y; // Converte uma variável string em number:
+console.log(z);
+//  se a variável não puder ser convertida, ela ainda se tornará um número, mas com o valor 'NaN' que é um number.
+
+//
+
+//    Converter Numbers em Strings:
+let num = 123;
+let str = String(num);
+console.log(typeof str);
+//  Pode ser usado para converter qualquer tipo de números, literais,variáveis ou expressões;
+console.log(String(123));
+console.log(String(100 + 23));
+
+//    Converter Date em Numbers:
+//    Converter Numbers em Date:
+//    Converter Booleans em Numbers:
+//    Converter Numbers em Booleans:
+
+//      Type Convertion JavaScript      //
+
+//  As variáveis JavaScript podem ser convertidas em uma nova variável e um outro tipo de dado:
+/*
+      Pelo uso de uma function;
+
+      Automaticamente pelo próprio JavaScript.
+*/
