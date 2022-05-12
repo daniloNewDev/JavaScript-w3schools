f94a05d84098dc7efe4f0017c139b3fdedefad67
Danilo Leite <danilo.cebolino@gmail.com>
2022-05-12 20:52:32 -0300

    add novo material curso javascript

---
+++ 19_bitwise/script.js
@@ -0,0 +1,3 @@
+//      Operadores 'Bitwise'      //
+
+//    em construção, por conta da grau de complexidade do material. É necessário compreender o conceito de números binários e o contexto dentro de JavaScript.
---
+++ 20_regular_expression/index.js
@@ -0,0 +1,14 @@
+//      Expressões Regulares      //
+
+//    Uma expressão regular é uma sequência de caracteres que forma um 'padrão de pesquisa'. Ao pesquisar dados em um texto, você pode usar esse padrão de pesquisa para descrever o que está procurando.
+
+//    Uma espressão regular pode ser um único caractere ou um padrão mais complicado. As espressões regulares podem ser usadas para executar todos os tipos de pesquisa de texto e operações de substituição de texto.
+/*
+      Sintaxe:
+        '/padrão/modificador'
+
+      exemplo : '/w3schools/i'
+*/
+//  Neste caso 'w3schools' é o padrão (para ser usado em uma pesquisa);
+
+//  E o 'i' é um modificador (modifica a pesquisa para não diferenciar maiúsculas de minúsculas).
